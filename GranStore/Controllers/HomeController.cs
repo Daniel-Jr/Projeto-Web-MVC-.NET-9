1729b2c05564dd4fb6435479d1063ecce26b6efb
Daniel Debrande Junior <daniel.debrande.jr@gmail.com>
2025-08-11 21:11:35 -0300

    Add Models And Controller

--- GranStore/Controllers/HomeController.cs
+++ GranStore/Controllers/HomeController.cs
@@ -4,18 +4,42 @@ using GranStore.Models;
 
 namespace GranStore.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<HomeController> _logger = logger;
 
     public IActionResult Index()
     {
-        return View();
+        Categoria categoria = new();
+
+        categoria.Id = 1;
+        categoria.Nome = "Produtos Diversos";
+
+        Categoria categoria1 = new()
+        {
+            Id = 1,
+            Nome = "Roupas"
+        };
+
+        Categoria categoria3 = new()
+        {
+            Id = 1,
+            Nome = "Sapatos"
+        };
+
+        List<Produto> produtos = [
+            new(){
+                Id = 1,
+                Nome = "Persiana",
+                Descricao = "Persiana com flores",
+                QtdeEstoque = 10,
+                ValorCusto = 113.23m,
+                ValorVenda = 230.33m,
+                Categoria = categoria,
+            }
+        ];
+ 
+        return View(categoria);
     }
 
     public IActionResult Privacy()
--- GranStore/Models/Categoria.cs
+++ GranStore/Models/Categoria.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace GranStore.Models
-{
-    public class Categoria
-    {
-        
-    }
+using System.ComponentModel.DataAnnotations;
+
+namespace GranStore.Models;
+
+public class Categoria
+{   
+    [Key]
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
 }
